6c5ec8c23720ebbda9daf69d260889e790fa5220
bmalkes <bmalkes@gmail.com>
2022-07-24 22:15:15 +0300

    feat:sidebar

--- src/components/SideBar/SiderBar.jsx
+++ src/components/SideBar/SiderBar.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import "./style.css";
+import { PencilLine } from "phosphor-react";
 
 const SideBar = () => {
-  return <aside className="siderBar">sidebar</aside>;
+  return (
+    <aside className="sideBar">
+      <img
+        className="cover"
+        src="https://images.unsplash.com/photo-1489549132488-d00b7eee80f1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80"
+      />
+      <div className="profile">
+        <img
+          src="https://images.unsplash.com/photo-1611643378160-39d6dd915b69?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=530&q=80"
+          alt=""
+        />
+        <strong>Bmalkes</strong>
+        <span>Front-end</span>
+        <footer>
+          <a href="#">
+            <PencilLine size={24} />
+            Edit Profile
+          </a>
+        </footer>
+      </div>
+    </aside>
+  );
 };
 
 export default SideBar;
